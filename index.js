2fa1dacecf62d59cda7fe90e9600f365566c15d8
brettmadrid <brettmadrid9@gmail.com>
2019-03-19 18:46:11 -0700

    post endpoint for students added

--- index.js
+++ index.js
@@ -11,77 +11,76 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.get("/api/students", async (req, res) => {
+// cohort endpoints
+server.get("/api/cohorts", async (req, res) => {
   try {
-    const students = await db("students"); // all the records
-    res.status(200).json(students);
+    const cohorts = await db("cohorts"); // all the records
+    res.status(200).json(cohorts);
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-
-// student endpoints
-server.get("/api/students/:id", async (req, res) => {
+server.get("/api/cohorts/:id", async (req, res) => {
   try {
-    const student = await db("students")
+    const cohort = await db("cohorts")
     .where({ id: req.params.id })
     .first();
-    res.status(200).json(student);
+    res.status(200).json(cohort);
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-// get all students by student
-server.get("/api/students/:id/students", async (req, res) => {
+// get all students by cohort
+server.get("/api/cohorts/:id/students", async (req, res) => {
   try {
-    const student = await db("students")
-    .join("students", "students.id", "=", "students.student_id")
-    .select("students.name", "students.name")
-    .where({ student_id: req.params.id })
-    res.status(200).json(student);
+    const cohort = await db("cohorts")
+    .join("students", "cohorts.id", "=", "students.cohort_id")
+    .select("cohorts.name", "students.name")
+    .where({ cohort_id: req.params.id })
+    res.status(200).json(cohort);
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-server.post("/api/students", async (req, res) => {
+server.post("/api/cohorts", async (req, res) => {
   try {
-    const [id] = await db("students").insert(req.body);
+    const [id] = await db("cohorts").insert(req.body);
 
-    const student = await db("students")
+    const cohort = await db("cohorts")
     .where({ id })
     .first();
 
-    res.status(201).json(student);
+    res.status(201).json(cohort);
   } catch (error) {
     const message = errors[error.errno] || "We ran into an error";
     res.status(500).json({ message, error });
   }
 });
 
-server.put("/api/students/:id", async (req, res) => {
+server.put("/api/cohorts/:id", async (req, res) => {
   try {
-    const count = await db("students")
+    const count = await db("cohorts")
     .where({ id: req.params.id })
       .update(req.body);
 
       if (count > 0) {
-        const student = await db("students")
+        const cohort = await db("cohorts")
         .where({ id: req.params.id })
         .first();
 
-      res.status(200).json(student);
+      res.status(200).json(cohort);
     } else {
       res.status(404).json({ message: "Records not found" });
     }
   } catch (error) {}
 });
 
-server.delete("/api/students/:id", async (req, res) => {
+server.delete("/api/cohorts/:id", async (req, res) => {
   try {
-    const count = await db("students")
+    const count = await db("cohorts")
       .where({ id: req.params.id })
       .del();
 
@@ -112,6 +111,21 @@ server.get("/api/students/:id", async (req, res) => {
     res.status(200).json(student);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+server.post("/api/students", async (req, res) => {
+  try {
+    const [id] = await db("students").insert(req.body);
+
+    const student = await db("students")
+    .where({ id })
+    .first();
+
+    res.status(201).json(student);
+  } catch (error) {
+    const message = errors[error.errno] || "We ran into an error";
+    res.status(500).json({ message, error });
   }
 });
 
